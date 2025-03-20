23f18541ca155e6859350315569d99df77129c5c
K-Rohit-Reddy <katikerohitreddy@gmail.com>
2025-03-20 15:04:29 +0530

    fixed download button issue

--- routes/upload.js
+++ routes/upload.js
@@ -84,7 +84,7 @@ router.post('/:shopId', upload.single('file'), async (req, res) => {
 // Route to handle file download
 router.get('/download/:filePath', async (req, res) => {
     const filePath = req.params.filePath;
-    const fullPath = path.join(__dirname, '..', 'uploads', filePath);
+    const fullPath = path.join(__dirname, '..', filePath);
 
     // Check if file exists
     if (!fs.existsSync(fullPath)) {
