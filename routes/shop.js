04f613a9646a61887e96eed540e881a0c001475b
K-Rohit-Reddy <katikerohitreddy@gmail.com>
2025-03-31 20:48:16 +0530

    added intent

--- routes/shop.js
+++ routes/shop.js
@@ -34,8 +34,8 @@ router.post('/register', async (req, res) => {
         });
         await newShop.save();
 
-        // Generate the QR code URL using the shop's ID
-        const qrData = `${BASE_FRONTEND_URL}/upload?shop_id=${newShop._id}`;
+        // Generate the QR code URL using the shop's ID and BASE_FRONTEND_URL
+        const qrData = `intent://${BASE_FRONTEND_URL}/upload?shop_id=${newShop._id}#Intent;scheme=https;package=com.android.chrome;S.browser_fallback_url=${BASE_FRONTEND_URL}/upload?shop_id=${newShop._id};end`;
         const qrCodeURL = await QRCode.toDataURL(qrData);
 
         console.log();
